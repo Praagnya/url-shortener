7263ef652fdbad97687406cb1e6c437ebb9157de
praagnya <praagnya@gmail.com>
2026-04-18 20:10:04 -0700

    add changes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import RedirectResponse
 import time 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, HttpUrl
 from functools import wraps
 from datetime import datetime 
 import secrets 
@@ -19,18 +20,18 @@ def timer(func):
     return wrapper
 
 class ShortenURLRequest(BaseModel):
-    original_url: str 
+    original_url: HttpUrl
 
 class ShortenURLResponse(BaseModel):
-    original_url: str 
-    code: str 
-    shortened_url: str
-    # created_at: datetime = Field(default_factory = datetime.now) (let us not show this to user)
+    original_url: HttpUrl
+    code: str
+    shortened_url: HttpUrl
 
 # Storage dict 
-secrets_db = {}
+secrets_db = {} # code to url
+logging_db = {} # code to list of click records
 
-def generate_short_code(original_url: str):
+def generate_short_code(original_url: HttpUrl):
     """Takes a URL and gives a secret code"""
     for code, url in secrets_db.items():
         if original_url == url:
@@ -40,8 +41,29 @@ def generate_short_code(original_url: str):
     return secret_code
 
 @app.post("/shorten")
+@timer
 def shorten_url(request: ShortenURLRequest):
     original_url = request.original_url
     secret_code = generate_short_code(original_url)
+    shortened_url = f"http://localhost:8000/{secret_code}"
+    return ShortenURLResponse(
+        original_url=original_url,
+        code=secret_code,
+        shortened_url=shortened_url
+    )
+
+
+@app.get("/{code}")
+@timer
+def redirect_to_new(code: str):
+    if not code in secrets_db: 
+        raise HTTPException(status_code=404, detail="Not found in DB")
+    else: 
+        # Log the click
+        if code not in logging_db:
+            logging_db[code] = []
+        logging_db[code].append(datetime.now())
+        return RedirectResponse(secrets_db[code])
+
 
 
