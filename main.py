be75a593b294232b9d9a4471618386f45361111d
praagnya <praagnya@gmail.com>
2026-04-30 01:47:05 -0700

    add files

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import RedirectResponse
-import time 
-from pydantic import BaseModel, Field, HttpUrl
+import time
+from pydantic import BaseModel, HttpUrl
 from functools import wraps
-from datetime import datetime 
-import secrets 
+from datetime import datetime
+import secrets
+from pathlib import Path
+import json
+from contextlib import asynccontextmanager
+
+# Storage dict 
+secrets_db = {} # code to url
+logging_db = {} # code to list of click records
+SECRETS_FILE = Path("secrets_db.json")
+LOGGING_FILE = Path("logging_db.json")
+
+def save_secrets_db():
+    serialize_secrets_db = {}
+    for code, url in secrets_db.items():
+        serialize_secrets_db[code] = str(url)
+    
+    with open(SECRETS_FILE, "w") as f: 
+        json.dump(serialize_secrets_db, f)
 
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    if SECRETS_FILE.exists():
+        with open(SECRETS_FILE, "r") as f:
+            secrets_db.update(json.load(f))
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 def timer(func):
     @wraps(func)
@@ -27,17 +52,15 @@ class ShortenURLResponse(BaseModel):
     code: str
     shortened_url: HttpUrl
 
-# Storage dict 
-secrets_db = {} # code to url
-logging_db = {} # code to list of click records
 
 def generate_short_code(original_url: HttpUrl):
     """Takes a URL and gives a secret code"""
     for code, url in secrets_db.items():
-        if original_url == url:
+        if str(original_url) == str(url):
             return code
     secret_code = secrets.token_urlsafe(6)
-    secrets_db[secret_code] = original_url
+    secrets_db[secret_code] = str(original_url)
+    save_secrets_db()
     return secret_code
 
 @app.post("/shorten")
@@ -46,6 +69,7 @@ def shorten_url(request: ShortenURLRequest):
     original_url = request.original_url
     secret_code = generate_short_code(original_url)
     shortened_url = f"http://localhost:8000/{secret_code}"
+
     return ShortenURLResponse(
         original_url=original_url,
         code=secret_code,
@@ -55,14 +79,14 @@ def shorten_url(request: ShortenURLRequest):
 
 @app.get("/{code}")
 @timer
-def redirect_to_new(code: str):
+def redirect_to_new(code: str, request: Request):
     if not code in secrets_db: 
         raise HTTPException(status_code=404, detail="Not found in DB")
     else: 
         # Log the click
         if code not in logging_db:
             logging_db[code] = []
-        logging_db[code].append(datetime.now())
+        logging_db[code].append({"timestamp": datetime.now(), "client_ip": request.client.host})
         return RedirectResponse(secrets_db[code])
 
 
